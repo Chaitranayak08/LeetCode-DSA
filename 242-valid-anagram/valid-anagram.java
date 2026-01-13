f4bff0d24a4d4ac769a15f79c3f456dee1eb4443
Chaitra Nayak <138785429+Chaitranayak08@users.noreply.github.com>
2026-01-13 14:36:10 +0530

    Time: 6 ms (42.20%) | Memory: 46.2 MB (47.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         if (s.length() != t.length()) return false;
-
-        int[] freq = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+         if(s.length()!=t.length()){
+            return false;
         }
+        s=s.toLowerCase();
+        t=t.toLowerCase();
 
-        for (int count : freq) {
-            if (count != 0) return false;
-        }
+        char[] c=s.toCharArray();
+        char[] d=t.toCharArray();
 
-        return true;
+       
+        Arrays.sort(c);
+        Arrays.sort(d);
+    
+        return Arrays.equals(c,d);
     }
 }
