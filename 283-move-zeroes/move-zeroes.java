4ce13c32acb751e36f8f834375081bd8e8ded834
Chaitra Nayak <138785429+Chaitranayak08@users.noreply.github.com>
2026-01-28 10:54:17 +0530

    Time: 2 ms (89.97%) | Memory: 48.1 MB (13.59%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       if(nums.length==1)
+            return ;
+        int first=0;
+       for(int i=0;i<nums.length;i++){
+      
+        if(nums[i]!=0){
+            nums[first]=nums[i];
+            first++; 
+        }
+      
+       }
+
+       while(first<nums.length){
+        nums[first]=0;
+        first++;
+       }
+ 
+    }
+}
